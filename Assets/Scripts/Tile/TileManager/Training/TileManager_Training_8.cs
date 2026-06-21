d7147a6938b99298acae65f71d03f670e2fb9baa
Harry Sanjaya <harrysanjaya262@gmail.com>
2026-06-21 20:15:26 +0700

    All training level

--- Assets/Scripts/Tile/TileManager/Training/TileManager_Training_8.cs
+++ Assets/Scripts/Tile/TileManager/Training/TileManager_Training_8.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TileManager_Training_8 : TileManager_Training
@@ -9,13 +10,33 @@ public class TileManager_Training_8 : TileManager_Training
 
     protected override void GenerateMapContent()
     {
+        int goalX = Random.Range(width / 2, width);
+
         for (int x = 0; x < width; x++)
         {
             SetTile(0, x, TileType.Floor, asFloorToo: true);
         }
 
-        int goalX = Random.Range(width / 2, width);
+        SetTile(0, goalX, TileType.Goal, false);
 
-        SetTile(0, goalX, TileType.Goal, asFloorToo: false);
+
+        int numberOfOres = Random.Range(4, 9);
+
+        List<int> availableSpaces = new List<int>();
+        for (int i = 1; i < width - 1; i++)
+        {
+            if (i == goalX) break;
+            availableSpaces.Add(i);
+        }
+
+        for (int i = 0; i < numberOfOres; i++)
+        {
+            int choosenIndex = Random.Range(0, availableSpaces.Count);
+            int x = availableSpaces[choosenIndex];
+
+            SetTile(0, x, TileType.WhiteOre, asFloorToo: false);
+
+            availableSpaces.RemoveAt(choosenIndex);
+        }
     }
 }
--- Assets/Scripts/Tile/TileManager/Training/TileManager_Training_9.cs
+++ Assets/Scripts/Tile/TileManager/Training/TileManager_Training_9.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 public class TileManager_Training_9 : TileManager_Training
 {
     public override void GenerateMap(bool setAllFloor = true)
@@ -13,6 +16,25 @@ public class TileManager_Training_9 : TileManager_Training
             {
                 SetTile(z, x, TileType.Floor, asFloorToo: true);
             }
+        }
+
+        List<Vector2Int> availableSpaces = new List<Vector2Int>();
+
+        for (int z = 0; z < length; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                availableSpaces.Add(new Vector2Int(z, x));
+            }
+        }
+
+        int oreCount = Random.Range(15, 26);
+        for (int i = 0; i < oreCount; i++)
+        {
+            int randomIndex = Random.Range(0, availableSpaces.Count);
+            Vector2Int randomPosition = availableSpaces[randomIndex];
+            availableSpaces.RemoveAt(randomIndex);
+            SetTile(randomPosition.x, randomPosition.y, TileType.WhiteOre, asFloorToo: false);
         }
     }
 }
